f247484a305412f9c36ad13e36fe541765fcfc87
wangjing <qigx92@163.com>
2019-09-09 00:02:50 +0800

    样式修改

--- src/pages/major/mine/index.vue
+++ src/pages/major/mine/index.vue
@@ -1,54 +1,109 @@
 <template>
   <div class="wrapper">
-    <div class="section">
-      <div class="summary">
-        <div class="score">123</div>
-        <div
-          @click="handleNavigate('/pages/sub/mine/score-detail/index')"
-          class="info"
-        >{{userInfo.nickName}}的大力丸 查看明细>></div>
-        <image :src="userInfo.avatarUrl" class="avator" />
-      </div>
+    <div class="summary">
+      <div v-if="userInfo.nickName" class="info">{{userInfo.nickName}}的大力丸</div>
+      <div class="score">123</div>
+      <div
+        @click="handleNavigate('/pages/sub/mine/score-detail/index')"
+        class="info"
+      >{{userInfo.nickName ? '查看明细>>' : '获取个人信息'}}</div>
+      <image :src="userInfo.avatarUrl" class="avator" />
     </div>
-    <div class="section">
-      <div class="header">已连续打卡1天</div>
-      <div class="content">
-        <div v-for="(item, index) in daysInfo" :key="index" class="day">
-          <div v-if="item.isSigned" class="day-content">biu</div>
-          <div v-else class="day-content">{{item.prizes}}</div>
-          <div class="day-footer">第{{index + 1}}天</div>
+    <div class="content">
+      <div class="section section-shadow">
+        <div class="section-header">
+          <div>
+            已连续打卡
+            <div class="red-text">1</div>天
+          </div>
         </div>
-      </div>
-      <div class="section section-center">
+        <div class="section-content">
+          <div v-for="(item, index) in daysInfo" :key="index" class="day">
+            <div v-if="item.isSigned" class="day-content">biu</div>
+            <div v-else class="day-content">{{item.prizes}}</div>
+            <div class="day-footer">第{{index + 1}}天</div>
+          </div>
+        </div>
         <div class="share-button">邀请好友立得100大力丸</div>
       </div>
-    </div>
-    <div class="section">
-      <div class="header">
-        奖品兑换
-        <div @click="handleNavigate('/pages/sub/mine/prizes-list/index')" class="header-more">更多奖品</div>
-      </div>
-      <div class="content prizes-item">
-        <div class="prizes-image">image</div>
-        <div class="prizes-content">
-          <div class="prizes-info">
-            <div class="prizes-title">空气加湿器</div>
-            <div class="prizes-price">
-              <div class="prizes-source-price">4500大力丸</div>3800
+      <div class="section section-shadow">
+        <div class="section-header">
+          奖品兑换
+          <div
+            @click="handleNavigate('/pages/sub/mine/prizes-list/index')"
+            class="header-more"
+          >更多奖品>></div>
+        </div>
+        <div class="section-content">
+          <scroll-view scroll-x>
+            <div class="scroll-horizontal">
+              <div class="prizes-item" @click="handleBooking()">
+                <image
+                  src="https://gw.alicdn.com/i1/49692482/O1CN01TtDPJz1UCnZ8cppZQ_!!49692482.jpg_300x10000Q75.jpg_.webp"
+                  class="prizes-image"
+                />
+                <div class="prizes-info">
+                  <div class="prizes-title">空气加湿器</div>
+                  <div class="prizes-price">
+                    <div class="prizes-source-price">4500大力丸</div>
+                    <div>3800</div>
+                  </div>
+                </div>
+              </div>
+              <div class="prizes-item" @click="handleBooking()">
+                <image
+                  src="https://gw.alicdn.com/i1/49692482/O1CN01TtDPJz1UCnZ8cppZQ_!!49692482.jpg_300x10000Q75.jpg_.webp"
+                  class="prizes-image"
+                />
+                <div class="prizes-info">
+                  <div class="prizes-title">空气加湿器</div>
+                  <div class="prizes-price">
+                    <div class="prizes-source-price">4500大力丸</div>
+                    <div>3800</div>
+                  </div>
+                </div>
+              </div>
+              <div class="prizes-item" @click="handleBooking()">
+                <image
+                  src="https://gw.alicdn.com/i1/49692482/O1CN01TtDPJz1UCnZ8cppZQ_!!49692482.jpg_300x10000Q75.jpg_.webp"
+                  class="prizes-image"
+                />
+                <div class="prizes-info">
+                  <div class="prizes-title">空气加湿器</div>
+                  <div class="prizes-price">
+                    <div class="prizes-source-price">4500大力丸</div>
+                    <div>3800</div>
+                  </div>
+                </div>
+              </div>
+              <div class="prizes-item" @click="handleBooking()">
+                <image
+                  src="https://gw.alicdn.com/i1/49692482/O1CN01TtDPJz1UCnZ8cppZQ_!!49692482.jpg_300x10000Q75.jpg_.webp"
+                  class="prizes-image"
+                />
+                <div class="prizes-info">
+                  <div class="prizes-title">空气加湿器</div>
+                  <div class="prizes-price">
+                    <div class="prizes-source-price">4500大力丸</div>
+                    <div>3800</div>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-          <div @click="handleBooking()" class="prizes-button">去兑换</div>
+          </scroll-view>
         </div>
       </div>
-    </div>
-    <div class="section">
-      <div v-for="item in menus" :key="item.url" class="list" @click="handleNavigate(item.url)">
-        <div class="item">
-          <div>
-            <div class="item-image">icon</div>
-            <div class="item-title">{{item.name}}</div>
+      <div class="section section-shadow">
+        <div class="section-header">其他工具</div>
+        <div class="section-content">
+          <div class="menu">
+            <div v-for="item in menus" :key="item.url" @click="handleNavigate(item.url)" class="menu-item">
+              <div class="menu-item-content">
+                <div class="menu-item-image">ic</div>
+                <div class="menu-item-title">{{item.name}}</div>
+              </div>
+            </div>
           </div>
-          <div class="icon">></div>
         </div>
       </div>
     </div>
@@ -60,7 +115,10 @@ export default {
   data() {
     return {
       canIUse: uni.canIUse("button.open-type.getUserInfo"),
-      userInfo: {},
+      userInfo: {
+        avatarUrl:
+          "https://gw.alicdn.com/tfs/TB1p.BGQXXXXXbFXFXXXXXXXXXX-160-160.png"
+      },
       menus: [
         {
           name: "兑换记录",
@@ -126,7 +184,7 @@ export default {
     handleBooking(orderId = 1) {
       uni.navigateTo({
         url: `/pages/sub/order/booking/index?orderId=${orderId}`
-      })
+      });
     },
     getUserInfo() {
       uni.getUserInfo({
@@ -146,109 +204,105 @@ export default {
 };
 </script>
 
-<style lang="scss">
-@import "@/styles/common.scss";
-</style>
-
 <style scoped lang="scss">
 .wrapper {
   background-color: #fff;
-  .section {
-    .summary {
-      position: relative;
-      background-image: linear-gradient(180deg, #ffb784, #fb6f72);
-      height: 290upx;
-      border-bottom-left-radius: 80upx;
-      border-bottom-right-radius: 80upx;
-      text-align: center;
-      box-sizing: border-box;
-      margin-bottom: 105upx;
-      .score {
-        font-size: 80upx;
-        line-height: 112upx;
-        color: #fff;
-        font-weight: 600;
-      }
-      .info {
-        color: #fff;
-        font-size: 26upx;
-        line-height: 36upx;
-      }
-      .avator {
-        position: absolute;
-        bottom: 0;
-        left: 50%;
-        transform: translate(-50%, 50%);
-        width: 210upx;
-        height: 210upx;
-        border-radius: 50%;
-        margin-top: 30upx;
-        border: 4upx solid #fff;
-      }
-    }
-    .header {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      color: #000;
-      font-size: 42upx;
-      font-weight: 500;
-      padding-left: 30upx;
-      padding-right: 30upx;
-      padding-bottom: 30upx;
-      .header-more {
-        font-size: 28upx;
-        color: #676c7c;
-      }
-    }
-    .content {
-      display: flex;
-      justify-content: space-between;
-      padding-left: 30upx;
-      padding-right: 30upx;
-      padding-bottom: 30upx;
-    }
-    .day {
-      color: #828282;
-      text-align: center;
-      .day-content {
-        border: 1px solid #828282;
-        border-radius: 50%;
-        width: 66upx;
-        height: 66upx;
-        line-height: 66upx;
-        font-size: 24upx;
-      }
-      .day-footer {
-        font-size: 20upx;
-        padding-top: 5upx;
-      }
-    }
-    .share-button {
-      display: flex;
-      justify-content: center;
-      align-items: center;
+  .summary {
+    position: relative;
+    background-image: linear-gradient(180deg, #ffb784, #fb6f72);
+    height: 290upx;
+    border-bottom-left-radius: 80upx;
+    border-bottom-right-radius: 80upx;
+    text-align: center;
+    box-sizing: border-box;
+    margin-bottom: 105upx;
+    .score {
+      font-size: 80upx;
+      line-height: 112upx;
       color: #fff;
-      font-size: 40upx;
-      width: 84vw;
-      height: 12.4vw;
-      background-color: #fb6f72;
-      border-radius: 6.2vw;
-      margin-top: 10upx;
+      font-weight: 600;
     }
-    .prizes-item {
-      .prizes-image {
-        width: 176upx;
-        height: 167upx;
-        border-radius: 8upx;
-        background-color: #f4f4f4;
-      }
-      .prizes-content {
+    .info {
+      color: #fff;
+      font-size: 26upx;
+      line-height: 36upx;
+    }
+    .avator {
+      position: absolute;
+      bottom: 0;
+      left: 50%;
+      transform: translate(-50%, 50%);
+      width: 210upx;
+      height: 210upx;
+      border-radius: 50%;
+      margin-top: 30upx;
+      border: 4upx solid #fff;
+    }
+  }
+  .content {
+    padding: 30upx;
+    .section {
+      display: flex;
+      flex-direction: column;
+      padding: 25upx 20upx;
+      box-sizing: border-box;
+      .section-header {
         display: flex;
         justify-content: space-between;
         align-items: center;
-        flex: 1;
-        padding-left: 20upx;
+        color: #000;
+        font-size: 36upx;
+        font-weight: 500;
+        margin-bottom: 25upx;
+        .header-more {
+          font-size: 28upx;
+          color: #676c7c;
+        }
+      }
+      .section-content {
+        display: flex;
+        justify-content: space-between;
+        &:not(:last-child) {
+          margin-bottom: 30upx;
+        }
+      }
+      .day {
+        color: #828282;
+        text-align: center;
+        .day-content {
+          border: 1px solid #828282;
+          border-radius: 50%;
+          width: 66upx;
+          height: 66upx;
+          line-height: 66upx;
+          font-size: 24upx;
+        }
+        .day-footer {
+          font-size: 20upx;
+          padding-top: 5upx;
+        }
+      }
+      .share-button {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        align-self: center;
+        color: #fff;
+        font-size: 40upx;
+        width: 84vw;
+        height: 12.4vw;
+        background-color: #fb6f72;
+        border-radius: 6.2vw;
+      }
+      .prizes-item {
+        display: inline-block;
+        width: 205upx;
+        .prizes-image {
+          width: 100%;
+          height: 165upx;
+          border-radius: 8upx;
+          background-color: #f4f4f4;
+        }
         .prizes-info {
           display: flex;
           flex-direction: column;
@@ -256,7 +310,7 @@ export default {
           justify-content: space-between;
           height: 100%;
           .prizes-title {
-            font-size: 32upx;
+            font-size: 28upx;
             color: #000;
             font-weight: 500;
           }
@@ -283,24 +337,55 @@ export default {
           font-size: 28upx;
           color: #fb6f72;
         }
+        &:not(:last-child) {
+          margin-right: 19upx;
+        }
+      }
+      .item-image {
+        display: inline-block;
+        width: 36upx;
+        height: 38upx;
+        margin-right: 20upx;
+      }
+      .icon {
+        color: #a4adc0;
+      }
+      .scroll-horizontal {
+        white-space: nowrap;
+      }
+      .menu {
+        flex: 1;
+        .menu-item {
+          display: inline-block;
+          width: 31%;
+          padding: 15upx;
+          background-color: #f4f4f4;
+          box-sizing: border-box;
+          .menu-item-content {
+            display: flex;
+            align-items: center;
+            color: #212830;
+            .menu-item-title {
+              font-size: 30upx;
+            }
+          }
+          &:not(:last-child) {
+            margin-right: 3.5%;
+          }
+        }
+      }
+      &.section-shadow {
+        box-shadow: 0 2upx 10upx 5upx rgba(0, 0, 0, 0.1);
+        border-radius: 20upx;
+      }
+      &:not(:last-child) {
+        margin-bottom: 40upx;
       }
     }
-    .item-image {
-      display: inline-block;
-      width: 36upx;
-      height: 38upx;
-      margin-right: 20upx;
-    }
-    .icon {
-      color: #a4adc0;
-    }
-    &.section-center {
-      display: flex;
-      justify-content: center;
-    }
-    &:not(:last-child) {
-      padding-bottom: 40upx;
-    }
+  }
+  .red-text {
+    display: inline-block;
+    color: #fb6f72;
   }
 }
 </style>
