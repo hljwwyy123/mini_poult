57cb64686045e33f636b0592352851b91145d0f5
Joy <hljwwyy123@gmail.com>
2019-10-10 08:44:33 +0800

    文案修改

--- src/pages/sub/mine/score-detail/index.vue
+++ src/pages/sub/mine/score-detail/index.vue
@@ -8,7 +8,7 @@
       <div class="header-content">
         <div class="count">{{userData.score}}</div>
         <div class="info">我的能量值余额</div>
-        <button open-type="share" class="button breath-animate">邀请好友赚100能量值</button>
+        <button open-type="share" class="button breath-animate">邀请好友得能量值</button>
       </div>
     </div>
     <scroll-view scroll-y class="content" @scrolltolower="handleLoadData">
